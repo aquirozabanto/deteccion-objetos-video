47a7e17ba4630689713d6d942afab09f5e67a0b7
aquirozabanto <31993349+aquirozabanto@users.noreply.github.com>
2020-09-13 02:52:29 -0500

    Update deteccion_video.py

--- deteccion_video.py
+++ deteccion_video.py
@@ -90,7 +90,8 @@ if __name__ == "__main__":
         for detection in detections:
             if detection is not None:
                 detection = rescale_boxes(detection, opt.img_size, RGBimg.shape[:2])
-                for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+                #for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+                for x1, y1, x2, y2, conf, cls_conf, classID, cls_pred in detection:
                     box_w = x2 - x1
                     box_h = y2 - y1
                     color = [int(c) for c in colors[int(cls_pred)]]
